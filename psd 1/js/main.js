59226a529fb4188c6a1efc5809849596015c21c4
Masud Rana <89349605+gitOfMasud@users.noreply.github.com>
2021-08-31 12:00:06 +0600

    Add files via upload

---
+++ psd 1/js/main.js
@@ -0,0 +1,9 @@
+
+    $(document).ready(function(){
+        $('.slider').bxSlider()
+        $('.main_menu').meanmenu({
+          meanScreenWidth: 771,
+          meanMenuContainer:'#mobile_menu'
+        });
+      });
+   
